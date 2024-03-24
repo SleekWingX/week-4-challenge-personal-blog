e33f64784504697517ce5250242bf26a8fdaaa62
Jack <alexisyazzie365@gmail.com>
2024-03-23 21:33:38 -0600

    Added working code to the blog page in javascript

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,17 +1,55 @@
 const backButton = document.querySelector('#back-button');
-// pull stored string data
-const storedData = localStorage.getItem('blogData');
+
 // convert string to javascript object
-const blogPost = JSON.parse(storedData);
+const blogListDiv = document.querySelector('#blog-list')
 
-console.log(blogPost)
-
-
+let blogList = [];
+/* document.getElementById('blog-title-1').textContent = blogPost.title;
+document.getElementById('blog-content-1').textContent = blogPost.content;
+document.getElementById('blog-author-1').textContent = ('Created by ' + blogPost.username); */
 // makes the back button take us back to form submission
 backButton.addEventListener('click', function(){
     window.location.href = 'index.html';
 })
 
+function init() {
+    // Get stored todos from localStorage
+    const storedData = JSON.parse(localStorage.getItem('blogData'));
+  
+    // If todos were retrieved from localStorage, update the todos array to it
+    if (storedData !== null) {
+      blogList = storedData;
+    }
+  
+    // This is a helper function that will render todos to the DOM
+    renderBlogs();
+  }
+
+function renderBlogs() {
+    // Render a new li for each todo
+    for (let i = 0; i < blogList.length; i++) {
+      const blogItem = blogList[i];
+  
+      const li = document.createElement('li');
+      li.setAttribute('class', "blog-element");
+  
+      const titleElement = document.createElement('h3');
+      titleElement.textContent = blogItem.title;
+
+      const contentElement = document.createElement('p');
+      contentElement.textContent = blogItem.content;
+
+      const usernameElement = document.createElement('p');
+      usernameElement.textContent = ('Created by ' + blogItem.username);
+  
+      li.appendChild(titleElement);
+      li.appendChild(contentElement);
+      li.appendChild(usernameElement);
+      blogListDiv.appendChild(li);
+    }
+  }
+
+  init();
 // Data Conversion is important
 // convert JSON String to JS OBJECT  --> JSON.parse(stringData)
 // convert JS ONject  to JSON Object  --> JSON.stringify(jsData)
--- assets/js/form.js
+++ assets/js/form.js
@@ -4,6 +4,29 @@ const usernameInput = document.querySelector('#username');
 const titleInput = document.querySelector('#title-name');
 const contentInput = document.querySelector('#content-text');
 
+let blogList = [];
+
+function init() {
+    
+    const storedBlog = JSON.parse(localStorage.getItem('blogData'));
+    
+    if (storedBlog !== null) {
+        blogList = storedBlog;
+    }
+}
+
+// checks if the form has been filled out (helped by Xpert)
+function checkFormCompletion() {
+    if (usernameInput.value.trim() === '') {
+        return false;
+    } else if (titleInput.value.trim() === '') {
+        return false;
+    } else if (contentInput.value.trim() === '') {
+        return false;
+    } else
+    // If all input fields are filled, return true
+    return true;
+}
 // Uses the submit button to update local storage and change the page
 submitForm.addEventListener('click', function(){
        //object to store form information
@@ -13,30 +36,21 @@ submitForm.addEventListener('click', function(){
         content: contentInput.value.trim(),
     };
 
-    // checks if the form has been filled out (helped by Xpert)
-    function checkFormCompletion() {
-        if (usernameInput.value.trim() === '') {
-            return false;
-        } else if (titleInput.value.trim() === '') {
-            return false;
-        } else if (contentInput.value.trim() === '') {
-            return false;
-        } else
-        // If all input fields are filled, return true
-        return true;
-    }
-
     let isFormComplete = checkFormCompletion();
     // logic to kick back if form boxes are empty
     if (isFormComplete != true) {
         alert("Please complete the form!")
     } else {
+    
+    blogList.push(blogStorage);    
     // command that stores the data to local storage
-    localStorage.setItem('blogData', JSON.stringify(blogStorage));
+    localStorage.setItem('blogData', JSON.stringify(blogList));
     // changes the page to blog
     window.location.href = 'blog.html'; 
     }
-})
+});
+
+init();
 
 // JSON is a STRING OBJECT
 /* jsonObj = {
